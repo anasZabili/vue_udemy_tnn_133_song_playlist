c43ab27ae32437eb96d886e271a6142b81a77c20
anasZabili <57200540+anasZabili@users.noreply.github.com>
2021-07-02 00:59:16 +0200

    Fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,7 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRs3uEi9yQf3D5eaosQM3NhLteYdFiUWA",
-  authDomain: "music-playlist-tnn-vue.firebaseapp.com",
-  projectId: "music-playlist-tnn-vue",
-  storageBucket: "music-playlist-tnn-vue.appspot.com",
-  messagingSenderId: "595909614759",
-  appId: "1:595909614759:web:aa98f4e23cec516f544f82",
+  x: "xxx"
 };
 
 // init firebase
